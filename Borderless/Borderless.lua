53dd17bb0f686891d46de35a9ba13a191a8f7e2a
Kristoffer Tverå <Bluekristoffer@gmail.com>
2018-07-21 21:48:48 +0200

    Target frame

--- Borderless/Borderless.lua
+++ Borderless/Borderless.lua
@@ -14,3 +14,14 @@ PlayerFrameAlternateManaBarLeftBorder:Hide();
 PlayerFrameAlternateManaBarRightBorder:Hide();
 PlayerFrameAlternateManaBarBorder:Hide();
 PlayerFrameTexture:SetAlpha(0);
+
+-- Position, scale, and style for TARGET frame
+TargetFrame:ClearAllPoints();
+TargetFrame:SetPoint("TOPLEFT", 820, -400);
+TargetFrame:SetScale(1.5);
+TargetFrame:SetUserPlaced(true);
+TargetFrame:StopMovingOrSizing();
+TargetFrame:SetMovable(false);
+TargetFrameTextureFrameTexture:Hide();
+TargetFrameSpellBar.Border:Hide();
+TargetFrameSpellBar:SetSize(103, 15);
