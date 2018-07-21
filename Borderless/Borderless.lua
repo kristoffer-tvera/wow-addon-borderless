eb82f82e1ff89574b3a114841b223dd50526a61a
Kristoffer Tverå <Bluekristoffer@gmail.com>
2018-07-21 21:57:21 +0200

    Background + box around every single action bar tile

--- Borderless/Borderless.lua
+++ Borderless/Borderless.lua
@@ -70,3 +70,129 @@ GameTimeFrame:Hide();
 --Objective tracker
 ObjectiveTrackerBlocksFrame.QuestHeader.Background:Hide();
 ObjectiveTrackerBlocksFrame.AchievementHeader.Background:Hide();
+
+
+--Removes the border/background of all action bar items:
+--Main Action Bar
+ActionButton1NormalTexture:Hide();
+ActionButton2NormalTexture:Hide();
+ActionButton3NormalTexture:Hide();
+ActionButton4NormalTexture:Hide();
+ActionButton5NormalTexture:Hide();
+ActionButton6NormalTexture:Hide();
+ActionButton7NormalTexture:Hide();
+ActionButton8NormalTexture:Hide();
+ActionButton9NormalTexture:Hide();
+ActionButton10NormalTexture:Hide();
+ActionButton11NormalTexture:Hide();
+ActionButton12NormalTexture:Hide();
+
+--MultiBarBottomLeft
+MultiBarBottomLeftButton1FloatingBG:Hide();
+MultiBarBottomLeftButton1NormalTexture:Hide();
+MultiBarBottomLeftButton2FloatingBG:Hide();
+MultiBarBottomLeftButton2NormalTexture:Hide();
+MultiBarBottomLeftButton3FloatingBG:Hide();
+MultiBarBottomLeftButton3NormalTexture:Hide();
+MultiBarBottomLeftButton4FloatingBG:Hide();
+MultiBarBottomLeftButton4NormalTexture:Hide();
+MultiBarBottomLeftButton5FloatingBG:Hide();
+MultiBarBottomLeftButton5NormalTexture:Hide();
+MultiBarBottomLeftButton6FloatingBG:Hide();
+MultiBarBottomLeftButton6NormalTexture:Hide();
+MultiBarBottomLeftButton7FloatingBG:Hide();
+MultiBarBottomLeftButton7NormalTexture:Hide();
+MultiBarBottomLeftButton8FloatingBG:Hide();
+MultiBarBottomLeftButton8NormalTexture:Hide();
+MultiBarBottomLeftButton9FloatingBG:Hide();
+MultiBarBottomLeftButton9NormalTexture:Hide();
+MultiBarBottomLeftButton10FloatingBG:Hide();
+MultiBarBottomLeftButton10NormalTexture:Hide();
+MultiBarBottomLeftButton11FloatingBG:Hide();
+MultiBarBottomLeftButton11NormalTexture:Hide();
+MultiBarBottomLeftButton12FloatingBG:Hide();
+MultiBarBottomLeftButton12NormalTexture:Hide();
+
+--BottomRight
+-- MultiBarBottomRightButton1FloatingBG:Hide();
+MultiBarBottomRightButton1NormalTexture:Hide();
+-- MultiBarBottomRightButton2FloatingBG:Hide();
+MultiBarBottomRightButton2NormalTexture:Hide();
+-- MultiBarBottomRightButton3FloatingBG:Hide();
+MultiBarBottomRightButton3NormalTexture:Hide();
+-- MultiBarBottomRightButton4FloatingBG:Hide();
+MultiBarBottomRightButton4NormalTexture:Hide();
+-- MultiBarBottomRightButton5FloatingBG:Hide();
+MultiBarBottomRightButton5NormalTexture:Hide();
+-- MultiBarBottomRightButton6FloatingBG:Hide();
+MultiBarBottomRightButton6NormalTexture:Hide();
+MultiBarBottomRightButton7FloatingBG:Hide();
+MultiBarBottomRightButton7NormalTexture:Hide();
+MultiBarBottomRightButton8FloatingBG:Hide();
+MultiBarBottomRightButton8NormalTexture:Hide();
+MultiBarBottomRightButton9FloatingBG:Hide();
+MultiBarBottomRightButton9NormalTexture:Hide();
+MultiBarBottomRightButton10FloatingBG:Hide();
+MultiBarBottomRightButton10NormalTexture:Hide();
+MultiBarBottomRightButton11FloatingBG:Hide();
+MultiBarBottomRightButton11NormalTexture:Hide();
+MultiBarBottomRightButton12FloatingBG:Hide();
+MultiBarBottomRightButton12NormalTexture:Hide();
+
+--Left
+MultiBarLeftButton1FloatingBG:Hide();
+MultiBarLeftButton1NormalTexture:Hide();
+MultiBarLeftButton2FloatingBG:Hide();
+MultiBarLeftButton2NormalTexture:Hide();
+MultiBarLeftButton3FloatingBG:Hide();
+MultiBarLeftButton3NormalTexture:Hide();
+MultiBarLeftButton4FloatingBG:Hide();
+MultiBarLeftButton4NormalTexture:Hide();
+MultiBarLeftButton5FloatingBG:Hide();
+MultiBarLeftButton5NormalTexture:Hide();
+MultiBarLeftButton6FloatingBG:Hide();
+MultiBarLeftButton6NormalTexture:Hide();
+MultiBarLeftButton7FloatingBG:Hide();
+MultiBarLeftButton7NormalTexture:Hide();
+MultiBarLeftButton8FloatingBG:Hide();
+MultiBarLeftButton8NormalTexture:Hide();
+MultiBarLeftButton9FloatingBG:Hide();
+MultiBarLeftButton9NormalTexture:Hide();
+MultiBarLeftButton10FloatingBG:Hide();
+MultiBarLeftButton10NormalTexture:Hide();
+MultiBarLeftButton11FloatingBG:Hide();
+MultiBarLeftButton11NormalTexture:Hide();
+MultiBarLeftButton12FloatingBG:Hide();
+MultiBarLeftButton12NormalTexture:Hide();
+
+--Right
+MultiBarRightButton1FloatingBG:Hide();
+MultiBarRightButton1NormalTexture:Hide();
+MultiBarRightButton2FloatingBG:Hide();
+MultiBarRightButton2NormalTexture:Hide();
+MultiBarRightButton3FloatingBG:Hide();
+MultiBarRightButton3NormalTexture:Hide();
+MultiBarRightButton4FloatingBG:Hide();
+MultiBarRightButton4NormalTexture:Hide();
+MultiBarRightButton5FloatingBG:Hide();
+MultiBarRightButton5NormalTexture:Hide();
+MultiBarRightButton6FloatingBG:Hide();
+MultiBarRightButton6NormalTexture:Hide();
+MultiBarRightButton7FloatingBG:Hide();
+MultiBarRightButton7NormalTexture:Hide();
+MultiBarRightButton8FloatingBG:Hide();
+MultiBarRightButton8NormalTexture:Hide();
+MultiBarRightButton9FloatingBG:Hide();
+MultiBarRightButton9NormalTexture:Hide();
+MultiBarRightButton10FloatingBG:Hide();
+MultiBarRightButton10NormalTexture:Hide();
+MultiBarRightButton11FloatingBG:Hide();
+MultiBarRightButton11NormalTexture:Hide();
+MultiBarRightButton12FloatingBG:Hide();
+MultiBarRightButton12NormalTexture:Hide();
+
+-- c# for generating blocks above,
+-- for(var i = 1; i <= 12; i++){
+	-- Console.WriteLine($"MultiBarLeftButton{i}FloatingBG:Hide();");
+	-- Console.WriteLine($"MultiBarLeftButton{i}NormalTexture:Hide();");
+-- }
