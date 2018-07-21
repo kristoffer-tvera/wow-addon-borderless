d8b46ff7ca1d06f2385411f29cd02da8bd8d9da0
Kristoffer Tverå <Bluekristoffer@gmail.com>
2018-07-21 21:48:32 +0200

    Player frame

--- Borderless/Borderless.lua
+++ Borderless/Borderless.lua
@@ -2,3 +2,15 @@
 MainMenuBarArtFrame.LeftEndCap:Hide();
 MainMenuBarArtFrame.RightEndCap:Hide();
 
+-- Position, scale, and style for PLAYER frame
+PlayerFrame:ClearAllPoints();
+PlayerFrame:SetPoint("TOPLEFT", 220, -400);
+PlayerFrame:SetScale(1.5);
+PlayerFrame:SetUserPlaced(true);
+PlayerFrame:StopMovingOrSizing();
+PlayerFrame:SetMovable(false);
+ComboPointPlayerFrame.Background:Hide();
+PlayerFrameAlternateManaBarLeftBorder:Hide();
+PlayerFrameAlternateManaBarRightBorder:Hide();
+PlayerFrameAlternateManaBarBorder:Hide();
+PlayerFrameTexture:SetAlpha(0);
