72f9c2876b854023c0d6730bfc0ea8ec84f0cf3c
Kristoffer Tverå <Bluekristoffer@gmail.com>
2018-07-22 20:42:46 +0200

    German localization

---
+++ Borderless/Borderless_localization.deDE.lua
@@ -0,0 +1,24 @@
+local _, L = ...;
+if GetLocale() == "deDE" then
+ L["Hello World!"] = "Hello Germany!";
+ L["No dragons/lions"] = "No dragons/lions";
+ L["Remove the dragons around the actionbars"] = "Remove the dragons around the actionbars";
+ L["No borders around actionbars"] = "No borders around actionbars";
+ L["Removes the borders around all the actionbar items"] = "Removes the borders around all the actionbar items";
+ L["No borders around minimap"] = "No borders around minimap";
+ L["Removes the borders around the minimap"] = "Removes the borders around the minimap";
+ L["No borders around the playerframe"] = "No borders around the playerframe";
+ L["Removes the border around the playerframe"] = "Removes the border around the playerframe";
+ L["No borders around the targetframe"] = "No borders around the targetframe";
+ L["Removes the border around the targetframe"] = "Removes the border around the targetframe";
+ L["No borders around the focusframe"] = "No borders around the focusframe";
+ L["Removes the border around the focusframe"] = "Removes the border around the focusframe";
+ L["No borders around the castbar"] = "No borders around the castbar";
+ L["Removes the border around the castbar"] = "Removes the border around the castbar";
+ L["No borders around the micro game menu"] = "No borders around the micro game menu";
+ L["Hide the bags"] = "Hide the bags";
+ L["Remove the bags over the ingame micro-menu line"] = "Remove the bags over the ingame micro-menu line";
+ L["Objective Tracker"] = "Objective Tracker";
+ L["Removes the border and header column effects of the objective tracker"] = "Removes the border and header column effects of the objective tracker";
+ L["borderless"] = "borderless";
+end
